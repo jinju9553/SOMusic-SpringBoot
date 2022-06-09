3804257c332cf8a1e30f62dfeb812024f72ddfc6
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2022-06-09 23:42:30 +0900

    220609 master backup

---
+++ src/main/java/com/example/SOMusic/domain/Account.java
@@ -0,0 +1,15 @@
+package com.example.SOMusic.domain;
+
+import lombok.*;
+
+@Getter @Setter 
+public class Account {
+
+	private String userId;
+	private String userName;
+	private String password;
+	private String address;
+	private int zipcod;
+	private String phone;
+	private String email;
+}
