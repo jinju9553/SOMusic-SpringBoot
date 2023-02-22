e543118ccd664a35803a22432e55685e411eaeda
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2023-02-22 17:10:07 +0900

    feat(repository) : AccountRepository 테스트 수정, PurchaseRepository 테스트 추가

--- src/test/java/com/example/SOMusic/repository/AccountRepositoryTest.java
+++ src/test/java/com/example/SOMusic/repository/AccountRepositoryTest.java
@@ -12,49 +12,47 @@ import java.util.List;
 @DataJpaTest
 public class AccountRepositoryTest {
 
+    private static final int LIST_SIZE = 2;
     @Autowired
     AccountRepository accountRepository;
+    Account account1 = createTestAccount();
+    Account account2 = createAnotherTestAccount();
 
     @Test
     void Account_저장_성공_테스트(){
-        Account account = createTestAccount();
+        Account result = accountRepository.save(account1);
 
-        Account result = accountRepository.save(account);
-
-        assertThat(result.getUserId()).isEqualTo(account.getUserId());
+        assertThat(result.getUserId()).isEqualTo(account1.getUserId());
     }
 
     @Test
     void Account_리스트_불러오기_테스트(){
-        Account account1 = createTestAccount();
-        Account account2 = createAnotherTestAccount();
         Account result1 = accountRepository.save(account1);
         Account result2 = accountRepository.save(account2);
 
         List<Account> accounts = accountRepository.findAll();
 
-        assertThat(accounts.size()).isEqualTo(2);
+        assertThat(accounts.size()).isEqualTo(LIST_SIZE);
     }
 
     @Test
     void 전화번호_이메일_계정찾기_테스트(){
-        Account account = createAnotherTestAccount();
-        Account result = accountRepository.save(account);
+        Account account = accountRepository.save(account2);
 
-        Account searchResult = accountRepository.findAccountByUserIdAndPhone("amy1234", "01011112222");
+        Account searchResult = accountRepository.findAccountByUserIdAndPhone(
+                "amy1234", "01011112222");
 
-        assertThat(searchResult.getUserName()).isEqualTo("amy");
+        assertThat(searchResult.getUserName()).isEqualTo(account.getUserName());
     }
 
     @Test
     void 아이디_전화번호_계정찾기_테스트(){
-        Account account = createAnotherTestAccount();
-        Account result = accountRepository.save(account);
+        Account account = accountRepository.save(account2);
 
         Account searchResult = accountRepository.findAccountByPhoneAndEmail(
                 "01011112222", "amy1234@gmail.com");
 
-        assertThat(searchResult.getUserName()).isEqualTo("amy");
+        assertThat(searchResult.getUserName()).isEqualTo(account.getUserName());
     }
 
     public Account createTestAccount(){
