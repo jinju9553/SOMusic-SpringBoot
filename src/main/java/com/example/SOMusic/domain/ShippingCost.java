fde23b6cfbc9b77f262fdf43aa3a002f8432d81d
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2023-03-08 17:55:22 +0900

    feat(enum): ShippingCost 클래스의 요소별 리스트를 반환하는 메소드 추가

--- src/main/java/com/example/SOMusic/domain/ShippingCost.java
+++ src/main/java/com/example/SOMusic/domain/ShippingCost.java
@@ -1,12 +1,12 @@
 package com.example.SOMusic.domain;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public enum ShippingCost {
-    SEMI_REGISTERED("준등기", 1, 1_800),
-    PARCEL("택배", 2, 3_000),
-    ISLAND_MOUNTAINOUS("택배(제주산간)", 3, 6_000),
-    EMPTY("", -1, -1);
+    SEMI_REGISTERED("준등기 (+1,800원)", 1, 1_800),
+    PARCEL("택배 (+3,000원)", 2, 3_000),
+    ISLAND_MOUNTAINOUS("택배(제주산간) (+6,000원)", 3, 6_000);
 
     private final String name;
     private final int code;
@@ -30,11 +30,38 @@ public enum ShippingCost {
         return cost;
     }
 
+    public static Object[] getNameList() {
+        Object[] result = Arrays.stream(ShippingCost.values())
+                .map(ShippingCost::getName)
+                .collect(Collectors.toList())
+                .toArray();
+
+        return result;
+    }
+
+    public static Object[] getCodeList() {
+        Object[] result = Arrays.stream(ShippingCost.values())
+                .map(ShippingCost::getCode)
+                .collect(Collectors.toList())
+                .toArray();
+
+        return result;
+    }
+
+    public static Object[] getCostList() {
+        Object[] result = Arrays.stream(ShippingCost.values())
+                .map(ShippingCost::getCost)
+                .collect(Collectors.toList())
+                .toArray();
+
+        return result;
+    }
+
     public static int findCostByCode(int code) {
         ShippingCost result = Arrays.stream(ShippingCost.values())
                 .filter(shippingCost -> shippingCost.code == code)
                 .findAny()
-                .orElse(EMPTY);
+                .get();
 
         return result.cost;
     }
