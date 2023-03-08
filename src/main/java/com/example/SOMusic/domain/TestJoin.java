ccd6bb2da0a09e1d1a39d13cfa59522096dddee1
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2023-03-08 17:56:20 +0900

    feat(domain): 테스트용 객체를 반환하는 클래스 정의

---
+++ src/main/java/com/example/SOMusic/domain/TestJoin.java
@@ -0,0 +1,36 @@
+package com.example.SOMusic.domain;
+
+import java.util.Date;
+
+public class TestJoin {
+
+    private static final int JOIN_ID = 67492;
+    private static final int GP_ID = 568;
+    private static final int GP_PRICE = 10_000;
+    private static final int SHIPPING_METHOD = 1;
+    private static final int SHIPPING_COST = ShippingCost.findCostByCode(SHIPPING_METHOD);
+    private static final int TOTAL_AMOUNT = 51_800;
+    private static final String CONSUMER_ID = "amy1234";
+
+    private static Join join = new Join();
+
+    public static Join createTestJoin() {
+        join.setJoinId(JOIN_ID);
+        join.setConsumerId(CONSUMER_ID);
+        join.setConsumerName("amy");
+        join.setConsumerBank("BankName");
+        join.setTotalAmount(TOTAL_AMOUNT);
+        join.setShippingMethod(SHIPPING_METHOD);
+        join.setShippingCost(SHIPPING_COST);
+
+        join.setRegDate(new Date());
+
+        GroupPurchase groupPurchase = new GroupPurchase();
+
+        groupPurchase.setGpId(GP_ID);
+        groupPurchase.setPrice(GP_PRICE);
+        join.setGroupPurchase(groupPurchase);
+
+        return join;
+    }
+}
---
+++ src/main/java/com/example/SOMusic/domain/TestPurchase.java
@@ -0,0 +1,43 @@
+package com.example.SOMusic.domain;
+
+import java.util.Date;
+
+public class TestPurchase {
+
+    private static final int PURCHASE_ID = 5723;
+    private static final int PRODUCT_ID = 173;
+    private static final String CONSUMER_ID = "mark123";
+    private static Purchase purchase = new Purchase();
+
+    public static Purchase createTestPurchase(){
+        purchase.setPurchaseId(PURCHASE_ID);
+        purchase.setConsumerId(CONSUMER_ID);
+        purchase.setConsumerName("mark");
+        purchase.setTotalAmount(3);
+        purchase.setStatus(1);
+        purchase.setShippingMethod(0);
+
+        purchase.setRegDate(new Date());
+
+        Product product = new Product();
+        product.setProductId(PRODUCT_ID);
+
+        purchase.setProduct(product);
+
+        return purchase;
+    }
+
+    public static Purchase createAnotherTestPurchase(){
+        purchase.setPurchaseId(PURCHASE_ID);
+        purchase.setConsumerId(CONSUMER_ID);
+        purchase.setConsumerName("mark");
+        purchase.setTotalAmount(8);
+        purchase.setStatus(2);
+        purchase.setShippingMethod(2);
+
+        purchase.setRegDate(new Date());
+        purchase.setProduct(new Product());
+
+        return purchase;
+    }
+}
