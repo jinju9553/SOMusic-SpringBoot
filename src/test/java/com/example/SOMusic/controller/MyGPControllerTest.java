fd3f0bc81063847b3d295e860b2a4946c7a5222a
jjuj99 <jjuj99@naver.com>
2023-03-15 00:17:02 +0900

    refactor:MyGPController 수정

--- src/test/java/com/example/SOMusic/controller/MyGPControllerTest.java
+++ src/test/java/com/example/SOMusic/controller/MyGPControllerTest.java
@@ -11,9 +11,9 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -25,6 +25,8 @@ import com.example.SOMusic.domain.Login;
 import com.example.SOMusic.service.AccountService;
 import com.example.SOMusic.service.GPService;
 import com.example.SOMusic.service.JoinService;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @WebMvcTest(MyGPController.class)
 class MyGPControllerTest {
@@ -45,6 +47,10 @@ class MyGPControllerTest {
 	
 	MockHttpSession session;
 	
+	private static final String MY_GP_INFO = "thyme/user/my/gp/myGPInfo";
+	private static final String MY_REGISTER_LIST = "thyme/user/my/gp/MyGPList";
+	private static final String MY_JOIN_LIST = "thyme/user/my/join/MyJoinList";
+	
 	@BeforeEach()
     public void setup() {
 		Account account = new Account();
@@ -60,87 +66,103 @@ class MyGPControllerTest {
 	
 	@Test
 	@DisplayName("공구 정보")
-	void info() throws Exception {
+	public void info() throws Exception {
 		
 		GroupPurchase gp = getGP();
+		String desc = "Is is<br> good";
 		
 		Mockito.when(gpSvc.getGP(1)).thenReturn(gp);
 		
-		mvc.perform(MockMvcRequestBuilders.get("/user/my/gp/info").param("gpId", "1").session(session))
-			.andDo(MockMvcResultHandlers.print())
-			.andExpect(MockMvcResultMatchers.model().attribute("gp", gp));//??????????????
-//			.andExpect(MockMvcResultMatchers.jsonPath("$.GroupPurchase.description").value("Is is<br> good!"));
-	}
-	
-	@Test
-	@DisplayName("등록한 공구 리스트 불러오기")
-	void gpList() throws Exception {
-				
-		List<GroupPurchase> gpList = getGPList();
-		
-		Mockito.when(gpSvc.getMyGPList("hi")).thenReturn(gpList);
-		
-		mvc.perform(MockMvcRequestBuilders.get("/user/my/gp/list").session(session))
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get("/user/my/gp/info").param("gpId", "1").session(session))
 			.andDo(MockMvcResultHandlers.print())
 			.andExpect(MockMvcResultMatchers.status().isOk())
-			.andExpect(MockMvcResultMatchers.model().attribute("gpList", gpList));
+			.andExpect(MockMvcResultMatchers.view().name(MY_GP_INFO))
+			.andExpect(MockMvcResultMatchers.model().attribute("gp", gp))
+			.andReturn();
+		
+		GroupPurchase result = (GroupPurchase) mvcResult.getModelAndView().getModel().get("gp");
+		assertThat(result.getDescription()).isEqualTo(desc);
 
 	}
 	
 	@Test
 	@DisplayName("등록한 공구 리스트 불러오기")
-	void joinList() throws Exception {
+	public void gpList() throws Exception {
+				
+		List<GroupPurchase> gpList = getGPList();
+		String userId = userSession.getAccount().getUserId();
 		
-		GroupPurchase gp = getGP();
+		Mockito.when(gpSvc.getMyGPList(userId)).thenReturn(gpList);
 		
-		Join join = new Join();
-		join.setJoinId(1);
-		join.setGroupPurchase(gp);
+		mvc.perform(MockMvcRequestBuilders.get("/user/my/gp/list").session(session))
+			.andDo(MockMvcResultHandlers.print())
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.view().name(MY_REGISTER_LIST))
+			.andExpect(MockMvcResultMatchers.model().attribute("gpList", gpList));
+
+	}
+	
+	@Test
+	@DisplayName("참여한 공구 리스트 불러오기")
+	public void joinList() throws Exception {
 		
-		List<Join> joinList = new ArrayList<>();
-		joinList.add(join);
+		List<Join> joinList = getJoinList();
+		String userId = userSession.getAccount().getUserId();
 		
-		Mockito.when(joinSvc.findAllByUserId("hi")).thenReturn(joinList);
+		Mockito.when(joinSvc.findAllByUserId(userId)).thenReturn(joinList);
 		
 		mvc.perform(MockMvcRequestBuilders.get("/user/my/join/list").session(session))
 			.andDo(MockMvcResultHandlers.print())
 			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.view().name(MY_JOIN_LIST))
 			.andExpect(MockMvcResultMatchers.model().attribute("joinList", joinList));
 
 	}
 	
 	@Test
 	@DisplayName("등록한 공구 상태 변경")
-	void updateJoinStatus() throws Exception {
+	public void updateJoinStatus() throws Exception {
 		
-		mvc.perform(MockMvcRequestBuilders.get("/user/my/join/status/update")
-										.param("gpId", "1").param("joinId", "1")
-										.param("status", "0").session(session))
+		int gpId = getGP().getGpId();
+		int joinId = getJoin().getJoinId();
+		int status = 2;
+		
+		Mockito.doNothing().when(joinSvc).updateStatus(joinId, status);
+		
+		mvc.perform(MockMvcRequestBuilders.post("/user/my/join/status/update")
+										.param("gpId", String.valueOf(gpId))
+										.param("joinId", String.valueOf(joinId))
+										.param("status", String.valueOf(status))
+										.session(session))
 			.andDo(MockMvcResultHandlers.print())
-			.andExpect(MockMvcResultMatchers.status().isOk());
+			.andExpect(MockMvcResultMatchers.redirectedUrl("/user/my/gp/info?gpId=" + gpId));
 			
-		Mockito.verify(joinSvc, Mockito.times(1)).updateStatus(1, 0);
+		Mockito.verify(joinSvc, Mockito.times(1)).updateStatus(gpId, status);
 		
 	}
 	
 	@Test
 	@DisplayName("등록한 공구 상태 일괄 변경")
-	void updateAllJoinStatus() throws Exception {
+	public void updateAllJoinStatus() throws Exception {
+		
+		int gpId = getGP().getGpId();
+		int status = 2;
 		
 		Mockito.doNothing().when(joinSvc).updateAllStatus(1, 2);
 		
 		mvc.perform(MockMvcRequestBuilders.post("/user/my/join/status/all/update")
-										.param("gpId", "1").param("status", "2")
+										.param("gpId", String.valueOf(gpId))
+										.param("status", String.valueOf(status))
 										.session(session))
 			.andDo(MockMvcResultHandlers.print())
 			.andExpect(MockMvcResultMatchers.redirectedUrl("/user/my/gp/info?gpId=1"));
 
-			
-		Mockito.verify(joinSvc, Mockito.times(1)).updateAllStatus(1, 2);
+		Mockito.verify(joinSvc, Mockito.times(1)).updateAllStatus(gpId, status);
 		
 	}
 	
 	public GroupPurchase getGP() {
+		
 		GroupPurchase gp = new GroupPurchase(1, "hi", "aaa", "link",
 				LocalDate.of(2023, 1, 1), LocalDate.of(2023, 1, 31),
 				"music", "3333", "bank", 1000, "Is is\n good");
@@ -149,17 +171,35 @@ class MyGPControllerTest {
 	}
 	
 	public List<GroupPurchase> getGPList() {
-		GroupPurchase gp1 = getGP();
 		
-		GroupPurchase gp2 = getGP();
-		gp2.setGpId(2);
-		gp2.setTitle("bbb");
+		GroupPurchase gp = getGP();
 		
 		List<GroupPurchase> gpList = new ArrayList<>();
-		gpList.add(gp1);
-		gpList.add(gp2);
+		gpList.add(gp);
 		
 		return gpList;
 	}
 	
+	public Join getJoin() {
+		
+		GroupPurchase gp = getGP();
+		String userId = userSession.getAccount().getUserId();
+		
+		Join join = new Join();
+		join.setJoinId(1);
+		join.setConsumerId(userId);
+		join.setGroupPurchase(gp);
+		
+		return join;
+	}
+	
+	public List<Join> getJoinList() {
+		
+		Join join = getJoin();
+		
+		List<Join> joinList = new ArrayList<>();
+		joinList.add(join);
+		
+		return joinList;
+	}
 }
