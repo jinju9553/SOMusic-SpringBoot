fdb83e72cc612db33cc6837738a38758c43312db
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2023-04-27 21:10:36 +0900

    feat(test): updateStatus() 테스트 함수 구현

--- src/test/java/com/example/SOMusic/service/JoinServiceTest.java
+++ src/test/java/com/example/SOMusic/service/JoinServiceTest.java
@@ -1,9 +1,10 @@
 package com.example.SOMusic.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.example.SOMusic.dao.JoinDao;
-import com.example.SOMusic.domain.*;
+import com.example.SOMusic.domain.GroupPurchase;
+import com.example.SOMusic.domain.Join;
+import com.example.SOMusic.domain.ShippingCost;
+import com.example.SOMusic.domain.TestJoin;
 import com.example.SOMusic.repository.JoinRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,9 +17,12 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.util.LinkedList;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @ExtendWith(SpringExtension.class)
 class JoinServiceTest {
 
+    private static final int STATUS = 3;
     private final ArgumentCaptor<Join> captor = ArgumentCaptor.forClass(Join.class);
 
     @InjectMocks
@@ -31,7 +35,7 @@ class JoinServiceTest {
     JoinDao joinDao;
 
     Join join1 = TestJoin.createTestJoin();
-    Join join2 = TestJoin.createTestJoin();
+    Join join2 = TestJoin.createAnotherTestJoin();
 
     @Test
     void registerJoin() {
@@ -46,8 +50,18 @@ class JoinServiceTest {
     }
 
     @Test
-    void modifyJoin() {
-        //TODO: 함수를 새로 설계하고 그 이후 테스트코드 작성
+    void modifyJoin() { //TODO: 학교 DB 복구 후 테스트
+        String initialName = join1.getConsumerName();
+        String initialBank = join1.getConsumerBank();
+        int initialShippingCost = join1.getShippingCost();
+        int initialAmount = join1.getTotalAmount();
+
+        joinService.modifyJoin(join1, join2);
+
+        assertThat(join1.getConsumerBank()).isNotEqualTo(initialBank);
+        assertThat(join1.getShippingCost()).isNotEqualTo(initialShippingCost);
+        assertThat(join1.getTotalAmount()).isNotEqualTo(initialAmount);
+        assertThat(join1.getConsumerName()).isEqualTo(initialName);
     }
 
     @Test
@@ -102,21 +116,6 @@ class JoinServiceTest {
     }
 
     @Test
-    void calculateTotal() {
-        GroupPurchase gp = join1.getGroupPurchase();
-
-        int price = gp.getPrice();
-        int quantity = join1.getQuantity();
-        int shippingCost = join1.getShippingCost();
-
-        int total = (price * quantity) + shippingCost;
-
-        int result = joinService.calculateTotal(gp, join1);
-
-        assertThat(result).isEqualTo(total);
-    }
-
-    @Test
     void initShippingCost() {
         int shippingMethod = join1.getShippingMethod();
         int shippingCost = ShippingCost.findCostByCode(shippingMethod);
@@ -127,7 +126,7 @@ class JoinServiceTest {
     }
 
     @Test
-    void updateTotal() {
+    void calculateTotal() {
         int newShippingMethod = 2;
         int totalAmount = join1.getTotalAmount();
         int shippingCost = join1.getShippingCost();
@@ -137,18 +136,46 @@ class JoinServiceTest {
         int newShippingCost = ShippingCost.findCostByCode(newShippingMethod);
         int newTotalPrice = (totalAmount - shippingCost) + newShippingCost;
 
-        int result = joinService.updateTotal(join1, newShippingCost);
+        int result = joinService.calculateUpdatedTotal(join1, newShippingCost);
 
         assertThat(result).isEqualTo(newTotalPrice);
     }
 
     @Test
     void updateAllStatus() {
-        //TODO: 함수를 새로 설계하고 그 이후 테스트코드 작성
+        List<Join> joins = initSameJoinList();
+
+        int initialStatus1 = joins.get(0).getStatus();
+        int initialStatus2 = joins.get(1).getStatus();
+
+        joinService.updateAllStatus(joins, STATUS);
+
+        int result1 = joins.get(0).getStatus();
+        int result2 = joins.get(1).getStatus();
+
+        assertThat(result1).isNotEqualTo(initialStatus1);
+        assertThat(result2).isNotEqualTo(initialStatus2);
+    }
+
+    List<Join> initSameJoinList() {
+        joinRepository.save(join1);
+        joinRepository.save(join1);
+
+        List<Join> joins = new LinkedList<>();
+        joins.add(join1);
+        joins.add(join1);
+
+        return joins;
     }
 
     @Test
     void updateStatus() {
-        //TODO: 함수를 새로 설계하고 그 이후 테스트코드 작성
+        int initialStatus = join1.getStatus();
+
+        joinService.updateStatus(join1, STATUS);
+
+        int result = join1.getStatus();
+
+        assertThat(result).isNotEqualTo(initialStatus);
     }
 }
