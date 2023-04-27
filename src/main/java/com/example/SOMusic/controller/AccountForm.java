8d93a93bc658933b2c59699313f1d26e19d706f5
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2023-04-27 21:42:05 +0900

    style(request): AccountForm 불필요한 주석 삭제

--- src/main/java/com/example/SOMusic/controller/AccountForm.java
+++ src/main/java/com/example/SOMusic/controller/AccountForm.java
@@ -1,31 +1,31 @@
 package com.example.SOMusic.controller;
 
-import java.io.Serializable;
-
 import com.example.SOMusic.domain.Account;
-
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+import java.io.Serializable;
+
+@Getter
+@Setter
 @SuppressWarnings("serial")
 public class AccountForm implements Serializable {
 
-	private Account account;
-	private boolean newAccount;
-	private String repeatedPassword;
-	
-	public AccountForm(Account account) { //회원정보 수정용 생성자
-		this.account = account;
-		this.newAccount = false;
-	}
+    private Account account;
+    private boolean newAccount;
+    private String repeatedPassword;
 
-	public AccountForm() { //회원정보 등록용 생성자
-		this.account = new Account();
-		this.newAccount = true;
-	}
-	
-	public boolean isNewAccount() {
-		return newAccount;
-	}
+    public AccountForm(Account account) {
+        this.account = account;
+        this.newAccount = false;
+    }
+
+    public AccountForm() {
+        this.account = new Account();
+        this.newAccount = true;
+    }
+
+    public boolean isNewAccount() {
+        return newAccount;
+    }
 }
